0d7591c79a8504019231aa8be6437a81aa4d9ffe
kalosius <kalosius43@gmail.com>
2025-03-27 12:44:04 +0300

    base page

--- schoolapp/urls.py
+++ schoolapp/urls.py
@@ -5,8 +5,18 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('gamesports/', views.gamesports, name='gamesports'),
+    path('clubsandactivities/', views.clubsandactivities, name='clubsandactivities'),
+    path('events/', views.events, name='events'),
+    path('schooladmin/', views.schooladmin, name='schooladmin'),
+    path('unebperformance/', views.unebperformance, name='unebperformance'),
+    path('beststudents/', views.beststudents, name='beststudents'),
+    path('admissions/', views.admissions, name='admissions'),
 ]
+
 # Images to load in the browser
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- schoolapp/views.py
+++ schoolapp/views.py
@@ -3,3 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/abouthistory.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def gallery(request):
+    return render(request, 'main/gallery.html')
+
+def gamesports(request):
+    return render(request, 'main/gamesports.html')
+
+def clubsandactivities(request):
+    return render(request, 'main/clubsandactivities.html')
+
+def events(request):
+    return render(request, 'main/events.html')
+
+def schooladmin(request):
+    return render(request, 'main/schooladmin.html')
+
+def unebperformance(request):
+    return render(request, 'main/unebperformance.html')
+
+def beststudents(request):
+    return render(request, 'main/beststudents.html')
+
+def admissions(request):
+    return render(request, 'main/admissions.html')
